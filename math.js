fd32551e9dc11f6eeb28ca79253246b4975b8243
Olivel Cantoran Gonzalez <ocg101010@gmail.com>
2023-03-01 14:38:14 -0600

    Agregando triangulo

--- math.js
+++ math.js
@@ -46,3 +46,52 @@ console.log({
 });
 
 console.groupEnd('Triangulo') 
+
+
+console.group('Circle') 
+
+const radioCirculo = 3;
+const diametroCirculo = radioCirculo * 2;
+const PI = 3.1415;
+
+const circunferencia = diametroCirculo * PI;
+const areaCirculo = (radioCirculo ** 2) * PI;
+
+console.log({
+radioCirculo,
+diametroCirculo,
+PI,
+circunferencia,
+areaCirculo,
+})
+
+function calcularCirculo(radio) {
+  const diametro = radio * 2;
+  const raadioAlCuadrado = Math.pow(radio, 2)
+
+  return {
+    circunferencia: diametro * Math.PI,
+    area: raadioAlCuadrado *  Math.PI.toFixed(),
+    diametro: radio * 2,
+  }
+}
+
+
+console.groupEnd('Circle') 
+
+
+
+console.group('Triangulo2') 
+
+function calcularAlturaTriangulo(lado1, base) {
+  if (lado1 === base) {
+    console.warn('Este no es un triangulo isoseles');
+  } else {
+    // h = raizcruadrada(lado1**2 - (b**) / 4) 
+    return Math.sqrt((lado1 ** 2) - ((base ** 2)) / 4);
+  }
+
+}
+  console.groupEnd('Triangulo2');
+
+ 
